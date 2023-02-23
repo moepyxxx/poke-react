c667a5515f9a49b2db8471ef2054e91ec8b448c0
moeko-iwai <moepyxxxxxx@gmail.com>
2023-02-23 18:39:04 +0900

    yupとreact-hook-formを使って名前をバリデーションした

--- app/pages/start.tsx
+++ app/pages/start.tsx
@@ -1,26 +1,52 @@
-import { useState } from "react";
-import { useAppDispatch, useAppSelector } from "hooks";
+import { useAppDispatch } from "hooks";
 import { setName } from "../stores/playerSlices";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type FormData = {
+  name: string;
+};
+
+const schema = yup
+  .object<FormData>({
+    name: yup
+      .string()
+      .min(1, "最小1文字です")
+      .max(5, "最大5文字です")
+      .matches(/^[ァ-ヶ]+$/i, "カタカナだけ利用できます")
+      .required("必須です"),
+  })
+  .required();
 
 export default function Start() {
   const dispatch = useAppDispatch();
   const router = useRouter();
-  const [nameInput, setNameInput] = useState<string>("");
-  const player = useAppSelector((state) => state.player);
 
-  const start = () => {
-    dispatch(setName(nameInput));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    dispatch(setName(data.name));
     router.push("/");
-  };
+  });
 
   return (
     <>
       <div>はじめから</div>
       <br />
-      <input value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
-      <button onClick={start}>はじめる</button>
-      <div>player: {player ? player.name : ""}</div>
+
+      <form onSubmit={onSubmit}>
+        <input {...register("name")} />
+        <p>{errors.name?.message}</p>
+        <input type="submit" />
+      </form>
     </>
   );
 }
